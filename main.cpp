75da276b2dda8579ac9f810c9ee88e7113546530
kepha-wiz <kephacheps55@gmail.com>
2025-04-05 16:06:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+using namespace std;
+int num1;
+int main(){
+  cout<<"Welcome to CKS library management system\n 
+    Enter the corresponding number to proceed"\n 1.Admin Login\n2.Student Login\n3.Exit<<endl;
+cin>>num1;
+}
 
