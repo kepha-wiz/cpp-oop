ed58c5683c73269d739c8474ba409f1e69ebe293
kepha-wiz <kephacheps55@gmail.com>
2025-04-05 16:12:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 int num1;
 int main(){
-  cout<<"Welcome to CKS library management system\n 
-    Enter the corresponding number to proceed"\n 1.Admin Login\n2.Student Login\n3.Exit<<endl;
+  cout<<"Welcome to CKS library management system\n Enter the corresponding number to proceed\n 1.Admin Login\n2.Student Login\n3.Exit"<<endl;
 cin>>num1;
 }
 
